89b3a73134da581e7dd182df3c2064603544bf18
deigo96 <58066161+deigo96@users.noreply.github.com>
2020-04-20 14:41:46 +0700

    Add files via upload

---
+++ daftar_buku.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <link rel="stylesheet" href="css/style.css">
+      <title>Perpustakaan Online</title>
+  </head>
+  <body>
+    <div id="container">
+        <div class="header"><img src="images/logo_ubsi.png" width="100" height="100"><h1 align="center">Perpustakaan Universitas BSI</h1></div>
+        <h4>Selamat Datang</h4>
+        <div class="main">
+          <div class="left">
+            <h3 align="center">MENU</h3>
+            <p>
+              <ul>
+                <li><a href="http://localhost/project_perpus/index.php">Logout</a></li>
+                <li><a href="http://localhost/project_perpus/home.php">Home</a></li>
+                <li><a href="#">Contact Us</a></li>
+                <li><input class="search" type="text" placeholder="Search..." required></li>
+              </ul>
+            </p>
+          </div>
+          <div class="right">
+            <h3 align="center">Daftar Buku Best Seller</h3>
+            <p>
+              <ul>
+                <li><a href="#">A Tale of Two Cities</a></li>
+                <li><a href="#">The Lord of The Ring</a></li>
+                <li><a href="#">Dream of The Read Chamber</a></li>
+              </ul>
+            </p>
+          </div>
+        </div>
+        <div class="middle2">
+          <h2 align="center">DAFTAR BUKU</h2><br>
+          <div class="buku">
+            <div class="foto">
+              <img src="images/a&d.jpg">
+              <div class="judul">Angles and Demons</div>
+              <div class="penulis">Dan Brown</div>
+            </div>
+          </div>
+          <div class="buku">
+            <div class="foto">
+              <img src="images/hobbit.jpg">
+              <div class="judul">The Hobbit</div>
+              <div class="penulis">J.R.R. Tolkien</div>
+            </div>
+          </div>
+          <div class="buku">
+            <div class="foto">
+              <img src="images/lord_of_the_ring.jpg">
+              <div class="judul">The Lord of The Ring</div>
+              <div class="penulis">J.R.R. Tolkien</div>
+            </div>
+          </div>
+          <div class="buku">
+            <div class="foto">
+              <img src="images/tales_two_cities.jpg">
+              <div class="judul">A Tale of Two Cities</div>
+              <div class="penulis">Charles Dickens</div>
+            </div>
+          </div>
+          <div class="buku">
+            <div class="foto">
+              <img src="images/TTOE.jpg">
+              <div class="judul">The Theory of Everything</div>
+              <div class="penulis">Stephen Hawking</div>
+            </div>
+          </div>
+        </div>
+        <div class="middle2">
+          <div class="buku">
+            <div class="foto">
+              <img src="images/red_chamber.jpg">
+              <div class="judul">Dream of The Read Chamber</div>
+              <div class="penulis">Cao Xueqin</div>
+            </div>
+          </div>
+          <div class="buku">
+            <div class="foto">
+              <img src="images/lionwitch.jpg">
+              <div class="judul">The Lion,The Witch and The Wardrobe</div>
+              <div class="penulis">C.S Lewis</div>
+            </div>
+          </div>
+          <div class="buku">
+            <div class="foto">
+              <img src="images/davincicode.jpg">
+              <div class="judul">The Davinci Code</div>
+              <div class="penulis">Dan Brown</div>
+            </div>
+          </div>
+          <div class="buku">
+            <div class="foto">
+              <img src="images/TGD.jpg">
+              <div class="judul">The God Delucion</div>
+              <div class="penulis">Richard Dawkins</div>
+            </div>
+          </div>
+          <div class="buku">
+            <div class="foto">
+              <img src="images/blackbeauty.jpg">
+              <div class="judul">Black Beauty</div>
+              <div class="penulis">Anna Sewell</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="footer"><p align="center">Copyright &copy 2020 - Belajar CSS Responsive</a></p>
+      </div>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <title>Perpustakaan Online</title>
+	  <script>
+		  var login = () => alert("Login Dulu Bos");
+	  </script>
+    </head>
+    <body>
+      <div id="container">
+        <div align="center" class="header"><img src="images/logo_ubsi.png" width="100" height="100"><h1 align="center">Perpustakaan Universitas BSI</h1>
+        </div>
+        <div class="main">
+          <div class="left">
+            <h3 align="center">MENU</h3>
+            <p>
+              <ul>
+                <li><a href="http://localhost/project_perpus/login.php">Login</a></li>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Contact Us</a></li>
+                <li><input class="search" type="text" placeholder="Search..." required></li>
+              </ul>
+            </p>
+          </div>
+          <div class="middle">
+            <h3 align="center">Buku Trending</h3>
+            <h2 align="center">The God Delucion</h2><br>
+            <div align="center">
+              <img src="images/TGD.jpg" width="300" >
+            </div>
+            <p><a href="#" onclick="login()">Baca Selengkapnya >></a></p>
+          </div>
+          <div class="right">
+            <h3 align="center">Daftar Buku Best Seller</h3>
+            <p>
+              <ul>
+                <li><a href="#" onclick="login()">A Tale of Two Cities</a></li>
+                <li><a href="#" onclick="login()">The Lord of The Ring</a></li>
+                <li><a href="#" onclick="login()">Dream of The Read Chamber</a></li>
+              </ul>
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="footer">
+        <p align="center">Copyright &copy; 2020 Belajar CSS Responsive </a></p>
+      </div>
+    </div>
+  </body>
+</html>
